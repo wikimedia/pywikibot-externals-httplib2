435717f722003fd2e2940d94a9e0e5d0b32d4a7d
John Vandenberg <jayvdb@gmail.com>
2014-08-04 15:30:28 +0700

    Fix nosetests breakage
    The last commit broke nosetests:
    
    File "__init__.py",
    line 7, in <module>
        del sys.modules['httplib2']
    KeyError: 'httplib2'
    
    Change-Id: Iff36327735b9e535bda2968ac17032efdaf76d24

--- __init__.py
+++ __init__.py
@@ -3,5 +3,6 @@ import sys
 
 path = os.path.dirname(__file__)+os.path.sep+'python'+str(sys.version_info[0])
 sys.path.insert(0, path)
-del sys.modules['httplib2']
+if 'httplib2' in sys.modules:
+    del sys.modules['httplib2']
 import httplib2
